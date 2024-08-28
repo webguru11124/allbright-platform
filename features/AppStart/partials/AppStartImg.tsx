d06eae2a1267b5c25170f97bd35a2f2273f82ea1
Robert Austin <austin437@hotmail.com>
2024-08-28 15:04:25 +0100

    fix: AppStartingImg updated to remove Link

--- features/AppStart/partials/AppStartImg.tsx
+++ features/AppStart/partials/AppStartImg.tsx
@@ -1,32 +1,28 @@
 import { Image } from "expo-image";
 import React from "react";
 
-import withAppStartImg from "../hocs/withAppStartImg";
 import { AppStartImgProps } from "@/features/AppStart/types";
-import Link from "@/components/Link";
+import withAppStartImg from "../hocs/withAppStartImg";
 
 const AppStartImg = ({ elm, xDim, yDim, c, idx }: AppStartImgProps) => (
-  <>
-    <Link href="/login" />
-    <Image
-      key={idx}
-      style={[
-        { position: "absolute", borderRadius: 50 },
-        {
-          left: xDim,
-          top: yDim,
-        },
-        { width: c, height: c },
-      ]}
-      source={elm.img}
-      transition={{
-        duration: 1000,
-        effect: "cross-dissolve",
-        timing: "ease-in",
-      }}
-      blurRadius={0.01}
-    />
-  </>
+  <Image
+    key={idx}
+    style={[
+      { position: "absolute", borderRadius: 50 },
+      {
+        left: xDim,
+        top: yDim,
+      },
+      { width: c, height: c },
+    ]}
+    source={elm.img}
+    transition={{
+      duration: 1000,
+      effect: "cross-dissolve",
+      timing: "ease-in",
+    }}
+    blurRadius={0.01}
+  />
 );
 
 export default withAppStartImg(AppStartImg);
