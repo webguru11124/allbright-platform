a7e384ecf187cb70affd950b530e7117d7693d0d
Robert Austin <austin437@hotmail.com>
2024-08-29 10:52:42 +0100

    refactor: app/(auth)/_layout.web.tsx created to remove Header component from web view

--- app/(auth)/_layout.web.tsx
+++ app/(auth)/_layout.web.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "@react-navigation/native";
 import { Slot } from "expo-router";
 
+import Header from "@/features/Auth/partials/Header";
 import { DefaultTheme } from "@/theme";
 
 export default function Layout() {
   return (
     <ThemeProvider value={DefaultTheme}>
+      <Header />
       <Slot />
     </ThemeProvider>
   );
