dfb51b2a8240297609c69a9cff132819d7e837bb
Robert Austin <austin437@hotmail.com>
2024-08-30 12:03:36 +0100

    feat: LoginForm updated with error validation

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,25 +1,31 @@
 import { TextInputProps, TextStyle } from "react-native";
 import { FloatingLabelInput } from "react-native-floating-label-input";
 
+import { CS } from "@/components/Typography";
 import withTheme from "@/hocs/withTheme";
 
-type Props = TextInputProps & { theme: Theme };
+type Props = TextInputProps & { error: string | undefined; theme: Theme };
 
-const TextInput = ({ theme, onChange, placeholder, ...rest }: Props) => {
+const TextInput = ({ theme, error, onChange, placeholder, ...rest }: Props) => {
   return (
-    <FloatingLabelInput
-      {...rest}
-      label={placeholder || "Please enter"}
-      inputStyles={{ outline: "none", fontSize: 16 } as TextStyle}
-      containerStyles={{
-        height: 50,
-        backgroundColor: theme.colors.inputs.background,
-        paddingLeft: 15,
-        borderRadius: 5,
-      }}
-      animationDuration={150}
-      onChange={onChange}
-    />
+    <>
+      <FloatingLabelInput
+        {...rest}
+        label={placeholder || "Please enter"}
+        inputStyles={{ outline: "none", fontSize: 16 } as TextStyle}
+        containerStyles={{
+          height: 50,
+          backgroundColor: theme.colors.inputs.background,
+          paddingLeft: 15,
+          borderRadius: 5,
+          borderColor: Boolean(error) ? "red" : "transparent",
+          borderWidth: Boolean(error) ? 3 : 0,
+        }}
+        animationDuration={150}
+        onChange={onChange}
+      />
+      {error && <CS color="red">{error}</CS>}
+    </>
   );
 };
 
--- features/Auth/Login/forms/LoginForm/index.tsx
+++ features/Auth/Login/forms/LoginForm/index.tsx
@@ -1,10 +1,43 @@
 import TextInput from "@/components/TextInput";
 import { SafeAreaView } from "react-native";
 
-import Space from "@/components/Space";
 import Button from "@/components/Button";
+import Space from "@/components/Space";
+import { useState } from "react";
+import { loginSchema } from "./loginSchema";
+
+type Inputs = {
+  email: string | undefined;
+  password: string | undefined;
+};
 
 const LoginForm = () => {
+  const [inputs, setInputs] = useState<Inputs>({
+    email: undefined,
+    password: undefined,
+  });
+
+  const [errors, setErrors] = useState<Inputs>({
+    email: undefined,
+    password: undefined,
+  });
+
+  const updateInputs = ({ name, value }: { name: string; value: any }) =>
+    setInputs((prev) => ({ ...prev, [name]: value }));
+
+  const validateInput = ({ name, value }: { name: string; value: any }) => {
+    const { error } = loginSchema.extract(name).validate(value);
+
+    setErrors((prev) => ({
+      ...prev,
+      [name]: error
+        ? `${error.message.replace("value", name.slice(0, 1).toUpperCase() + name.slice(1))}`
+        : undefined,
+    }));
+  };
+
+  const onPress = () => {};
+
   return (
     <SafeAreaView>
       <TextInput
@@ -12,6 +45,15 @@ const LoginForm = () => {
         placeholderTextColor={"#ddd"}
         inputMode="email"
         textContentType="emailAddress"
+        value={inputs.email}
+        error={errors.email}
+        onBlur={() => validateInput({ name: "email", value: inputs["email"] })}
+        onChange={(ev) =>
+          updateInputs({
+            name: "email",
+            value: (ev.target as unknown as HTMLInputElement).value,
+          })
+        }
       />
       <Space height={10} />
       <TextInput
@@ -19,9 +61,20 @@ const LoginForm = () => {
         placeholderTextColor={"#ddd"}
         inputMode="text"
         textContentType="password"
+        value={inputs.password}
+        error={errors.password}
+        onBlur={() =>
+          validateInput({ name: "password", value: inputs["password"] })
+        }
+        onChange={(ev) =>
+          updateInputs({
+            name: "password",
+            value: (ev.target as unknown as HTMLInputElement).value,
+          })
+        }
       />
       <Space height={50} />
-      <Button>Submit</Button>
+      <Button onPress={onPress}>Submit</Button>
     </SafeAreaView>
   );
 };
