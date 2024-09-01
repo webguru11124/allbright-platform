b01120f328334f6d0800371e72f5ad0403b07bab
Robert Austin <austin437@hotmail.com>
2024-09-01 17:29:04 +0100

    feat: Button modified to show spinner on loading

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,11 @@
-import { ButtonProps } from "react-native";
+import { ButtonProps, ActivityIndicator } from "react-native";
 import styled from "styled-components/native";
 
 import { CM } from "@/components/Typography";
 import withTheme from "@/hocs/withTheme";
 
 type Props = Omit<ButtonProps, "title"> & {
+  isLoading: boolean;
   theme: Theme;
   children: React.ReactNode;
 };
@@ -12,10 +13,19 @@ type Props = Omit<ButtonProps, "title"> & {
 const Button = (props: Props) => {
   return (
     <StyledPressable
-      disabled={props.disabled}
+      disabled={props.disabled || props.isLoading}
       onPress={props.onPress}
       theme={props.theme}>
-      <CM color={props.theme.colors.button.primary}>{props.children}</CM>
+      <CM color={props.theme.colors.button.primary}>
+        {props.isLoading ? (
+          <ActivityIndicator
+            size={24}
+            color={props.theme.colors.button.primary}
+          />
+        ) : (
+          props.children
+        )}
+      </CM>
     </StyledPressable>
   );
 };
