ec369d42425796f62a87cad01322cbe297937c80
Robert Austin <austin437@hotmail.com>
2024-09-04 15:52:50 +0100

    feat(CountryPicker): StyleSheets converted to styled-components

--- forms/components/CountryPicker/index.tsx
+++ forms/components/CountryPicker/index.tsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
-import { Alert, Modal, Pressable, StyleSheet, View } from "react-native";
-import styled from "styled-components/native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { useState } from "react";
+import { Alert, Modal, TextInputProps } from "react-native";
+import styled from "styled-components/native";
 
 import { CM } from "@/components/Typography";
 import withTheme from "@/hocs/withTheme";
 
-type Props = { error: string | undefined; theme: Theme };
+type Props = TextInputProps & { error: string | undefined; theme: Theme };
 
-const CountryPicker = ({ theme, error }: Props) => {
+const CountryPicker = ({
+  theme,
+  error,
+  onBlur,
+  onChangeText,
+  placeholder,
+  value,
+  placeholderTextColor,
+}: Props) => {
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
-    <View>
+    <>
       <Modal
         animationType="slide"
         transparent={true}
@@ -21,22 +29,20 @@ const CountryPicker = ({ theme, error }: Props) => {
           Alert.alert("Modal has been closed.");
           setModalVisible(!modalVisible);
         }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <CM style={styles.modalText}>Hello World!</CM>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}>
-              <CM style={styles.textStyle}>Hide Modal</CM>
-            </Pressable>
-          </View>
-        </View>
+        <CenteredView>
+          <ModalView>
+            <ModalText>Hello World!</ModalText>
+            <Button onPress={() => setModalVisible(!modalVisible)}>
+              <Text>Hide Modal</Text>
+            </Button>
+          </ModalView>
+        </CenteredView>
       </Modal>
       <StyledPressable theme={theme} onPress={() => setModalVisible(true)}>
         <CM color={"rgb(73, 101, 140)"}>Country</CM>
         <MaterialIcons name={"arrow-drop-down"} size={24} color={"black"} />
       </StyledPressable>
-    </View>
+    </>
   );
 };
 
@@ -53,51 +59,42 @@ const StyledPressable = styled.Pressable`
   color: rgb(73, 101, 140);
 `;
 
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    height: "80%",
-    width: "100%",
-    backgroundColor: "white",
-    borderRadius: 20,
-    borderTopRightRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 5,
-    elevation: 10,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-  },
-});
+const CenteredView = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: 22px;
+`;
+
+const ModalView = styled.View`
+  margin: 20px;
+  height: 80%;
+  width: 100%;
+  background-color: #eee;
+  border-radius: 20px;
+  padding: 35px;
+  align-items: center;
+  shadow-color: #000;
+  box-shadow: 0px 0px 15px #17171750;
+  elevation: 3;
+`;
+
+const Button = styled.Pressable`
+  border-radius: 20px;
+  padding: 10px;
+  elevation: 2;
+  background-color: #2196f3;
+`;
+
+const Text = styled(CM)`
+  color: #fff;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const ModalText = styled(CM)`
+  margin-bottom: 15px;
+  text-align: center;
+`;
 
 export default withTheme(CountryPicker);
