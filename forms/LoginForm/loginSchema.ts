2ea84b57545aeb24eda865d4996459108ad77351
tiarama <c92macleod@gmail.com>
2024-09-18 12:38:33 +0100

    Joi - allow special characters in password

--- forms/LoginForm/loginSchema.ts
+++ forms/LoginForm/loginSchema.ts
@@ -4,7 +4,7 @@ export const loginSchema = {
   email: Joi.string()
     .email({ tlds: { allow: false } })
     .required(),
-  password: Joi.string().min(4).alphanum().required(),
+  password: Joi.string().min(4).required(),
 };
 
 export default loginSchema;
