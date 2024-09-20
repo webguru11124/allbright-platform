f4d17550baa7af6ff4254e28005f2557873c1965
Robert Austin <austin437@hotmail.com>
2024-09-20 16:03:26 +0100

    refactor: useRegisterForm updated

--- forms/hooks/useRegisterForm.ts
+++ forms/hooks/useRegisterForm.ts
@@ -10,10 +10,19 @@ import {
   useSignIn,
 } from "@/hooks/resources/useAuth";
 import { setToken } from "@/utils/token";
+import { useCallback, useEffect, useState } from "react";
+
+enum State {
+  IDLE = 1,
+  REGISTER,
+  SIGNIN,
+  SUCCESS,
+}
 
 const useRegisterForm = (
   registerSchema: Joi.PartialSchemaMap<any> | undefined,
 ) => {
+  const [state, setState] = useState<State>(State.IDLE);
   const {
     inputs,
     postBody,
@@ -31,28 +40,60 @@ const useRegisterForm = (
 
   const { mutate: mutateSignin, isPending: isPendingSignIn } = useSignIn();
 
-  const signin = () =>
-    mutateSignin(postBody as Login, {
-      onSuccess: (response) => {
-        setToken(response as unknown as string);
-        router.replace("/home");
-      },
-      onError: (error: any) => showErrorMessage(error.message),
-    });
-
   const onPress = () => {
-    if (isFormValid) {
+    if (isFormValid) setState(State.REGISTER);
+  };
+
+  const register = useCallback(
+    () =>
       mutateRegister(
         registrationAdaptor(postBody as RegisterInput) as Register,
         {
           onSuccess: ({ data }) => {
-            if (data.success) signin();
+            if (data.success) setState(State.SIGNIN);
           },
           onError: (error: any) => showErrorMessage(error.message),
         },
-      );
+      ),
+    [mutateRegister, postBody, showErrorMessage],
+  );
+
+  const signin = useCallback(
+    () =>
+      mutateSignin(postBody as Login, {
+        onSuccess: (response) => {
+          setToken(response as unknown as string);
+          setState(State.SUCCESS);
+        },
+        onError: (error: any) => showErrorMessage(error.message),
+      }),
+    [mutateSignin, postBody, showErrorMessage],
+  );
+
+  const navigateToHome = useCallback(() => {
+    router.replace("/home");
+  }, []);
+
+  useEffect(() => {
+    switch (state) {
+      case State.REGISTER:
+        if (isPendingRegister === false) register();
+        break;
+      case State.SIGNIN:
+        if (isPendingSignIn === false) signin();
+        break;
+      case State.SUCCESS:
+        navigateToHome();
+        break;
     }
-  };
+  }, [
+    isPendingRegister,
+    isPendingSignIn,
+    navigateToHome,
+    register,
+    signin,
+    state,
+  ]);
 
   return {
     inputs,
