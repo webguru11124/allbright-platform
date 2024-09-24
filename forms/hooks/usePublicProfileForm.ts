3f22715281a42159361f9d5785876a65e679d8b9
Viktor Zashev <viktor.zashev@allbright.co>
2024-09-24 11:16:07 +0200

    chore: Fix lint issues
    Refs: #13

--- forms/hooks/usePublicProfileForm.ts
+++ forms/hooks/usePublicProfileForm.ts
@@ -1,50 +1,22 @@
-import { router } from "expo-router";
 import Joi from "joi";
 
-import { RegisterInput, registrationAdaptor } from "@/forms/adaptors";
 import useFormWithPassConf from "@/forms/hooks/useFormWithPassConf";
-import {
-  Login,
-  Register,
-  useRegister,
-  useSignIn,
-} from "@/hooks/resources/useAuth";
-import { setToken } from "@/utils/token";
-import { useCallback, useEffect, useState } from "react";
-
-enum State {
-  IDLE = 1,
-  REGISTER,
-  SIGNIN,
-  SUCCESS,
-}
 
 const usePublicProfileForm = (
   publicProfileSchema: Joi.PartialSchemaMap<any> | undefined,
 ) => {
-  const [state, setState] = useState<State>(State.IDLE);
-  const {
-    inputs,
-    postBody,
-    errors,
-    blurFuncs,
-    changeTextFuncs,
-    isFormValid,
-    showErrorMessage,
-  } = useFormWithPassConf(publicProfileSchema, {
-    // default: {},
-  });
+  const { inputs, errors, blurFuncs, changeTextFuncs, isFormValid } =
+    useFormWithPassConf(publicProfileSchema, {
+      // default: {},
+    });
 
   // TODO: update mutation to use usePublicProfile API
-  const { mutate: mutateRegister, isPending } =
-    useRegister();
 
   const onPress = () => {
-    // if (isFormValid) setState(State.REGISTER); 
+    // if (isFormValid) setState(State.REGISTER);
     console.log("mutate calling");
     // TODO: Update handling error and sucess on mutate
   };
-
 
   return {
     inputs,
@@ -52,7 +24,6 @@ const usePublicProfileForm = (
     blurFuncs,
     changeTextFuncs,
     isFormValid,
-    isPending,
     onPress,
   };
 };
