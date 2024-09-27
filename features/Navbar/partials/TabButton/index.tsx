6b5a9beb432f081b5a6f313bac3121c012665f04
Robert Austin <austin437@hotmail.com>
2024-09-27 15:45:51 +0100

    refactor: TabButton refactored using the Container/Component pattern

---
+++ features/Navbar/partials/TabButton/index.tsx
@@ -0,0 +1,37 @@
+import { Href } from "expo-router";
+import { Animated } from "react-native";
+import useTabButton from "../../hooks/useTabButton";
+import TabButton from "./TabButton";
+
+export type Props = {
+  isActive: boolean;
+  widthAnim: Animated.Value;
+  href: Href<string | object>;
+  onHoverIn: MyMouseEvent;
+  onHoverOut: MyMouseEvent;
+  children: React.ReactNode;
+};
+
+const TabButtonContainer = ({
+  href,
+  children,
+  ...rest
+}: Pick<Props, "href" | "children">) => {
+  const { isActive, widthAnim, onHoverIn, onHoverOut } = useTabButton({
+    href: href,
+  });
+
+  return (
+    <TabButton
+      isActive={isActive}
+      widthAnim={widthAnim}
+      href={href}
+      onHoverIn={onHoverIn}
+      onHoverOut={onHoverOut}
+      {...rest}>
+      {children}
+    </TabButton>
+  );
+};
+
+export default TabButtonContainer;
