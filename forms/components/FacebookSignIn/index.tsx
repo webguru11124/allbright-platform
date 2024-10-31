66a649b8ad3599653be7f5b9f1bd5954b5f7fa0e
Robert Austin <austin437@hotmail.com>
2024-10-31 16:32:13 +0000

    feat: FacebookLogin button styled

--- forms/components/FacebookSignIn/index.tsx
+++ forms/components/FacebookSignIn/index.tsx
@@ -1,15 +1,45 @@
+import MaterialIcons from "@expo/vector-icons/FontAwesome";
 import React from "react";
 import styled from "styled-components/native";
 
 import Button from "@/forms/components/Button";
 
 const FacebookSignIn = ({ onPress }: { onPress: GestureEvent }) => {
-  return <StyledButton onPress={onPress}>Sign in with Facebook</StyledButton>;
+  return (
+    <StyledButton onPress={onPress}>
+      <VerticalCenter>
+        <FacebookIcon
+          name={"facebook-square"}
+          size={24}
+          color={"white"}
+        />
+        Login with <Bold>Facebook</Bold>
+      </VerticalCenter>
+    </StyledButton>
+  );
 };
 
 const StyledButton = styled(Button)`
+  width: 396px;
+  height: 45px;
   margin-vertical: 8px;
-  width: 100%;
+  background-color: #5890ff;
+  border-radius: 4px;
+  text-align: center;
+`;
+
+const VerticalCenter = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FacebookIcon = styled(MaterialIcons)`
+  margin-right: 10px;
+`;
+
+const Bold = styled.Text`
+  font-weight: 800;
 `;
 
 export default FacebookSignIn;
