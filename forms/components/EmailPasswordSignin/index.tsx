08a13b96e3e119489ac3565d3c3b685f3852f7ae
Robert Austin <austin437@hotmail.com>
2024-10-31 17:06:21 +0000

    feat: EmailPasswordLogin button styled

--- forms/components/EmailPasswordSignin/index.tsx
+++ forms/components/EmailPasswordSignin/index.tsx
@@ -1,17 +1,44 @@
+import { Image } from "expo-image";
 import React from "react";
 import styled from "styled-components/native";
 
 import Button from "@/forms/components/Button";
 
-const EmailPasswordSignIn = ({ onPress }: { onPress: GestureEvent }) => {
+const EmailPasswordSignin = ({ onPress }: { onPress: GestureEvent }) => {
   return (
-    <StyledButton onPress={onPress}>Sign in with Email/Password</StyledButton>
+    <StyledButton onPress={onPress}>
+      <VerticalCenter>
+        <AllBrightIcon source={require("@/assets/images/icon.png")} />
+        Sign in with Email and Password
+      </VerticalCenter>
+    </StyledButton>
   );
 };
 
 const StyledButton = styled(Button)`
-  margin-vertical: 8px;
-  width: 100%;
+  width: 396px;
+  height: 45px;
+  margin-bottom: 8px;
+  border-radius: 4px;
+  text-align: center;
 `;
 
-export default EmailPasswordSignIn;
+const AllBrightIcon = styled(Image)`
+  width: 25px;
+  height: 25px;
+  margin-right: 10px;
+`;
+
+const VerticalCenter = styled.View`
+  position: absolute;
+  top: 0;
+  left: -150;
+  right: 0;
+  bottom: 0;
+  width: 300px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default EmailPasswordSignin;
