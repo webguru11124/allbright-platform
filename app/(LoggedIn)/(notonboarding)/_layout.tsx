87b8a16f267c661872e524772bd805b5d682fccb
Robert Austin <austin437@hotmail.com>
2024-11-29 16:15:01 +0000

    feat: Added image to Drawer

--- app/(LoggedIn)/(notonboarding)/_layout.tsx
+++ app/(LoggedIn)/(notonboarding)/_layout.tsx
@@ -1,24 +1,49 @@
+import { DrawerContent } from "@react-navigation/drawer";
 import { Drawer } from "expo-router/drawer";
+import React from "react";
+import { View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import IsOnboarding from "@/components/IsOnboarding";
+import { H4 } from "@/components/Typography";
+import { UserContext } from "@/contexts/UserContext";
+import TabImage from "@/features/Navbar/partials/TabImage";
 import useTheme from "@/hooks/useTheme";
+import { UserModel } from "@/types/user";
 
 export default function Layout() {
+  const { user } = React.useContext<{
+    user: Partial<UserModel> | undefined;
+    refetch: Function;
+  }>(UserContext);
   const theme = useTheme();
   return (
     <IsOnboarding>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <Drawer
-          screenOptions={{
-            headerStatusBarHeight: 0,
-            headerStyle: {
-              backgroundColor: theme.colors.background,
-            },
-            headerTintColor: theme.colors.primary,
-            headerTitleStyle: {
-              color: theme.colors.primary,
-            },
+          drawerContent={(props) => {
+            return (
+              <>
+                <View
+                  style={{
+                    backgroundColor: theme.colors.background,
+                    padding: 20,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "flex-start",
+                  }}>
+                  <TabImage
+                    imageSrc={user?.imageSrc}
+                    initials={
+                      (user?.firstName?.slice(0, 1) || "A") +
+                      (user?.lastName?.slice(0, 1) || "B")
+                    }
+                  />
+                  <H4 style={{ paddingLeft: 10 }}>{user?.name}</H4>
+                </View>
+                <DrawerContent {...props}></DrawerContent>
+              </>
+            );
           }}>
           <Drawer.Screen
             name="home"
@@ -32,6 +57,12 @@ export default function Layout() {
             options={{
               drawerLabel: "Connect",
               title: "Connect",
+            }}
+          />
+          <Drawer.Screen
+            name="account"
+            options={{
+              drawerItemStyle: { display: "none" },
             }}
           />
         </Drawer>
