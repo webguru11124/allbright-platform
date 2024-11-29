8ff1421a98210eb3783abbf1c5a9d3a519a63aac
Viktor Zashev <viktor.zashev.pro@gmail.com>
2024-11-29 16:05:06 +0100

    fix: ethincGroup adaptor

--- forms/adaptors/index.ts
+++ forms/adaptors/index.ts
@@ -157,3 +157,24 @@ export type PrivateProfileOutput = {
   dateOfBirth: Date;
   ethnicGroups: string[];
 };
+
+export const ethnicGroupsAdapter = (
+  ethnics: string[]
+): (typeof ethnicGroups)[number][] => {
+  const ethnicGroupsData: (typeof ethnicGroups)[number][] = [];
+  for (const ethnic of ethnics) {
+    if (ethnic === "Prefer not to say") {
+      ethnicGroupsData.push(ethnicGroups[0]);
+    } else {
+      const ethicGroup = ethnicGroups.find((e) => e.title === ethnic);
+      if (!ethicGroup)
+        ethnicGroupsData.push({
+          title: "Other",
+          value: ethnic as any,
+          id: "ca4564ca-55fd-11ee-8c99-0242ac120002",
+        });
+      else ethnicGroupsData.push(ethicGroup);
+    }
+  }
+  return ethnicGroupsData;
+};
