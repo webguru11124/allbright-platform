158a1dc5d21f4c85e1edc716d3c7faa41059728d
Robert Austin <austin437@hotmail.com>
2024-12-05 17:17:32 +0000

    feat: BusinessCardAdaptor created

---
+++ features/BusinessCard/adaptors/index.ts
@@ -0,0 +1,19 @@
+import { BusinessCard } from "@/types/businessCard/BusinessCard";
+import { UserModel } from "@/types/user";
+
+export const businessCardAdaptor = (user: UserModel): BusinessCard => {
+  return {
+    id: user.id || user.uid,
+    displayName: user.name || `${user.firstName} ${user.lastName}`,
+    imageSrc: user.imageSrc || user.profileImageUrl,
+    businessCardColor: user?.businessCardColor || "#FFFFFF",
+    location: `${[user.city, user.country].filter(Boolean).join(", ")}` || user.location,
+    job: user.jobTitle,
+    jobLevel: user.jobLevel,
+    jobIndustry: user.jobIndustry,
+    jobCompany: user.jobCompany,
+    displayPhoto: user.displayPhoto !== false,
+    goals: user.goals,
+    interests: user.interests,
+  };
+};
