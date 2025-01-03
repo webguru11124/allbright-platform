2fa7e44cb4a2abc0ab43cff145f12ddb409ce8bd
Robert Austin <austin437@hotmail.com>
2025-01-03 08:57:55 +0000

    refactor: Login_(Desktop|Mobile) updated to remove styled-components

--- features/Auth/Login/Login_Mobile.tsx
+++ features/Auth/Login/Login_Mobile.tsx
@@ -1,31 +1,33 @@
-import styled from "styled-components/native";
+import { StyleSheet, View } from "react-native";
 
 import RegisterLink from "@/features/Auth/partials/RegisterLink";
 import LoginForm from "@/forms/LoginForm";
 
 const LoginMobile = () => {
   return (
-    <Main>
-      <FormContainer>
+    <View style={[styles.main]}>
+      <View style={[styles.formContainer]}>
         <LoginForm />
-      </FormContainer>
+      </View>
       <RegisterLink />
-    </Main>
+    </View>
   );
 };
 
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 50,
+  },
+  formContainer: {
+    flex: 1,
+    maxWidth: 360,
+    paddingTop: 30,
+    paddingHorizontal: 10,
+    alignItems: "flex-start",
+  },
+});
+
 export default LoginMobile;
-
-const Main = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  padding-bottom: 50px;
-`;
-
-const FormContainer = styled.View`
-  flex: 1;
-  max-width: 360px;
-  padding: 30px 10px 0 10px;
-  align-items: flex-start;
-`;
