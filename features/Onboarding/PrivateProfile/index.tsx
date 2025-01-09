c9acaf9b1fc59c9134139d0ef7e7a2a1ad8fd487
Robert Austin <austin437@hotmail.com>
2025-01-09 08:26:29 +0000

    refactor: PrivateProfile updated to remove colors object

--- features/Onboarding/PrivateProfile/index.tsx
+++ features/Onboarding/PrivateProfile/index.tsx
@@ -1,13 +1,17 @@
 import { CM, H3, H5 } from "@/components/Typography";
 import { OnboadingPageLayout } from "@/features/Onboarding/OnboardingLayout";
 import PrivateProfileForm from "@/forms/PrivateProfileForm";
-import colors from "@/theme";
+import withTheme from "@/hocs/withTheme";
 
-const PrivateProfile = () => {
+type Props = {
+  theme: Theme;
+};
+
+const PrivateProfile = ({ theme }: Props) => {
   return (
     <OnboadingPageLayout>
       <H3>Section 1: Private Profile</H3>
-      <H5 color={colors.teal}>Step 3 of 4</H5>
+      <H5 style={{ color: theme.colors.txt.subtitle }}>Step 3 of 4</H5>
       <CM>
         This information will never be shown to anyone except AllBright. We will use it to tailor your experience on the
         platform.
@@ -17,4 +21,4 @@ const PrivateProfile = () => {
   );
 };
 
-export default PrivateProfile;
+export default withTheme(PrivateProfile);
