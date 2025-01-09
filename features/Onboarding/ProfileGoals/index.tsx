69ce44d65a1b122b5b5ed3c2565a25e974588d85
Robert Austin <austin437@hotmail.com>
2025-01-09 08:32:50 +0000

    refactor: ProfileGoals updated to remove colors object

--- features/Onboarding/ProfileGoals/index.tsx
+++ features/Onboarding/ProfileGoals/index.tsx
@@ -1,13 +1,17 @@
 import { CM, H3, H5 } from "@/components/Typography";
 import { OnboadingPageLayout } from "@/features/Onboarding/OnboardingLayout";
 import ProfileGoalsForm from "@/forms/ProfileGoalsForm";
-import colors from "@/theme";
+import withTheme from "@/hocs/withTheme";
 
-const ProfileGoals = () => {
+type Props = {
+  theme: Theme;
+};
+
+const ProfileGoals = ({ theme }: Props) => {
   return (
     <OnboadingPageLayout>
       <H3>Section 3: Your Career Goals</H3>
-      <H5 color={colors.teal}>Step 4 of 4</H5>
+      <H5 style={{ color: theme.colors.txt.subtitle }}>Step 4 of 4</H5>
       <CM>
         This information will never be shown to anyone except AllBright. We will use it to tailor your experience on the
         platform.
@@ -17,4 +21,4 @@ const ProfileGoals = () => {
   );
 };
 
-export default ProfileGoals;
+export default withTheme(ProfileGoals);
